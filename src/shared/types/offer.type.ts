baaccd87fb64480e05ac7a564a97d8a0a141a42d
Бойко Владимир <boiko.vladimir@urfu.me>
2025-05-03 02:18:49 +0500

    Добавил CLI с базовыми командами

---
+++ src/shared/types/offer.type.ts
@@ -0,0 +1,25 @@
+import { City } from './city.type.js';
+import { Convenience } from './convenience.type.js';
+import { Coordinates } from './coordinates.type.js';
+import { OfferType } from './offer-type.enum.js';
+import { User } from './user.type.js';
+
+export type Offer = {
+    title: string;
+    publicationDate: Date;
+    description: string;
+    city: City;
+    imagePreview: string;
+    rating: number;
+    images: string[];
+    isFavorite: boolean;
+    isPremium: boolean;
+    conveniences: Convenience[];
+    rooms: number;
+    guests: number;
+    price: number;
+    type: OfferType;
+    author: User;
+    commentsCount: number;
+    coordinates: Coordinates;
+  };
