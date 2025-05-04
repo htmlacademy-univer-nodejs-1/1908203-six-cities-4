0b6ac2170e109dc160e64dbf37775c5ea504f3fe
Бойко Владимир <boiko.vladimir@urfu.me>
2025-05-04 18:17:16 +0500

    Пофиксил ворнинги линтера

--- src/shared/modules/coordinates/coordinates.entity.ts
+++ src/shared/modules/coordinates/coordinates.entity.ts
@@ -1,5 +1,5 @@
-import { prop } from "@typegoose/typegoose";
-import { Coordinates } from "../../types/coordinates.type.js";
+import { prop } from '@typegoose/typegoose';
+import { Coordinates } from '../../types/coordinates.type.js';
 
 export class CoordinatesEntity implements Coordinates {
   @prop()
--- src/shared/modules/user/default-user.service.ts
+++ src/shared/modules/user/default-user.service.ts
@@ -11,7 +11,7 @@ export class DefaultUserService implements UserService {
   constructor(
     @inject(Component.Logger) private readonly logger: Logger,
     @inject(Component.UserModel) private readonly userModel: types.ModelType<UserEntity>
-  ) {}
+  ) { }
 
   public async create(dto: CreateUserDto, salt: string): Promise<DocumentType<UserEntity>> {
     const user = new UserEntity(dto);
@@ -19,12 +19,12 @@ export class DefaultUserService implements UserService {
 
     const result = await this.userModel.create(user);
     this.logger.info(`New user created: ${user.email}`);
-    
+
     return result;
   }
 
   public async findByEmail(email: string): Promise<DocumentType<UserEntity> | null> {
-    return this.userModel.findOne({email});
+    return this.userModel.findOne({ email });
   }
 
   public async findOrCreate(dto: CreateUserDto, salt: string): Promise<DocumentType<UserEntity>> {
